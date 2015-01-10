9cffe9b88ddc8d31b79282b0c907768d4ddbcaf0
Brittany Arthur <brittany_arthur@hotmail.com>
2015-01-09 23:19:25 -0800

    improved Twilio integration using cool libraries

--- hack/views.py
+++ hack/views.py
@@ -3,13 +3,18 @@ from django.views.generic.base import TemplateView
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
+#new imports, twil update
+from django_twilio.decorators import twilio_view
+from twilio.twiml import Response
+
 class IndexView(TemplateView):
   template_name = 'index.html'
 
 def test(request):
 	return render_to_response('base.html', {'test': 1})
 
-@csrf_exempt
+@twilio_view
 def sms(request):
-	twiml = '<Response><Message>Hey Study Student!</Message></Response>'
-	return HttpResponse(twiml, content_type='text/xml')
+	r = Response()
+	r.message('Hello from your Django app!')
+	return r
