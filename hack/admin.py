f5aced5b7ede5f810e7d45f7a71851d2e3645a7c
Brittany Arthur <brittany_arthur@hotmail.com>
2015-01-10 01:25:41 -0800

    working progress on login

--- hack/admin.py
+++ hack/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+# Import the UserProfile model individually.
+from hack.models import UserProfile
 
 # Register your models here.
+admin.site.register(UserProfile)
--- hack/models.py
+++ hack/models.py
@@ -2,3 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    # This line is required. Links UserProfile to a User model instance.
+    user = models.OneToOneField(User)
+
+    # The additional attributes we wish to include.
+    phone = models.CharField(max_length = 30)
+    email_alerts = models.IntegerField(default=1) # default true
+    text_alerts = models.IntegerField(default=1)
+
+    # Override the __unicode__() method to return out something meaningful!
+    def __unicode__(self):
+        return self.user.username
